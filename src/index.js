3bbbfd65488c69ccc45a77709b838e3376076926
Danilo Salvador <danilosalvador@Mac-mini-de-Danilo.local>
2020-05-16 18:09:47 -0300

    chore:Add Nodemon

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/test', (request, response) => {
-  return response.json({ message:'OK'});
+  return response.json({ message:'OK with Nodemon'});
 });
 
 app.listen(3333, () => {
